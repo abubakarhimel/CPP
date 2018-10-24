4cae1f83ac977449251a88c7e8c25d7a469b1952
Md Abu Bakar Siddique <himel104@gmail.com>
2018-10-24 03:08:03 +0200

    MyClass Class added

---
+++ cpp_git/src/MyClass.cpp
@@ -0,0 +1,14 @@
+/*
+ * MyClass.cpp
+ *
+ *  Created on: Oct 24, 2018
+ *      Author: Himel
+ */
+
+#include "MyClass.h"
+
+MyClass::MyClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ cpp_git/src/MyClass.h
@@ -0,0 +1,16 @@
+/*
+ * MyClass.h
+ *
+ *  Created on: Oct 24, 2018
+ *      Author: Himel
+ */
+
+#ifndef MYCLASS_H_
+#define MYCLASS_H_
+
+class MyClass {
+public:
+	MyClass();
+};
+
+#endif /* MYCLASS_H_ */
